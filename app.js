a0b25a37bfe7371cee5ee92a6ad6607e3aac0fdd
Allan <aavendan@gmail.com>
2023-01-02 11:57:22 -0500

    database

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var fotosRouter = require('./routes/fotos');
+var etiquetasRouter = require('./routes/etiquetas');
+var fotosRESTRouter = require('./rest/fotos');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/fotos', fotosRouter);
+app.use('/etiquetas', etiquetasRouter);
+app.use('/rest/fotos', fotosRESTRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
