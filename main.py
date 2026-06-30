1989a778f4fb1fed1c63ab9164bff670f7f26e95
Allan <aavendan@gmail.com>
2026-06-30 17:59:37 +0000

    Agregar lógica para obtener ítems con paginación y ajustar la base de datos simulada

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "Qux"}, {"item_name": "Quux"}, {"item_name": "Corge"}, {"item_name": "Grault"}, {"item_name": "Garply"}, {"item_name": "Waldo"}, {"item_name": "Fred"}, {"item_name": "Plugh"}, {"item_name": "Xyzzy"}, {"item_name": "Thud"}]
+
+
 app = FastAPI()
 
 @app.get("/")
@@ -9,3 +12,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
    return {"item_id": item_id}
+
+@app.get("/items/")
+def read_item(skip: int = 0, limit: int = 10, q: str | None = None):
+   results = fake_items_db[skip : skip + limit]
+   return results
